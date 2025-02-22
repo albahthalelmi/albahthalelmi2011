41ecb58f20136fa72d54da383a39fc450c23e95f
albahthalelmi <albahthalelmi4@gmail.com>
2025-02-22 22:17:07 +0300

    Add files via upload

---
+++ activity.js
@@ -0,0 +1,14 @@
+function toggleList() {
+        var listContent = document.getElementById("listItems");
+        listContent.classList.toggle("show");
+    }
+
+    // إغلاق القائمة عند الضغط في أي مكان خارجها
+    document.addEventListener("click", function(event) {
+        var listContainer = document.querySelector(".list-container");
+        var listContent = document.getElementById("listItems");
+
+        if (!listContainer.contains(event.target)) {
+            listContent.classList.remove("show");
+        }
+    });
